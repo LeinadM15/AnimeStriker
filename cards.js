93c7680662fd13d914f2b8ccdcf2bc716eca1695
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-06-23 23:22:00 +0200

    Update Mark Arion and Meca Mark names, set Mark Arion to Oro, add CB as secondary position for all Mark cards

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const DB_VERSION = '190';
+const DB_VERSION = '191';
 
 const tsubasaAll = [
     ...tsubasaCards,
