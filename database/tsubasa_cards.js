81978314de1ddbbcd5eaf41a4ee0ec47d078c0e5
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-06-26 15:48:42 +0200

    Add Dada Silva cards

--- database/tsubasa_cards.js
+++ database/tsubasa_cards.js
@@ -12623,5 +12623,47 @@ const nathanCards = [
         teamIcon: "teams/Tottenham.png",
         image: "assets/characters/Inglaterra/AdamBlakePrime.png",
         background: "assets/Cartas/Trophy.png"
+    },
+    {
+        id: "dada_oro",
+        name: "DADA SILVA",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 88,
+        position: "ST",
+        secondaryPositions: [],
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/br.png",
+        teamIcon: "teams/Chelsea.png",
+        image: "assets/characters/Brasil/DadaOro.png",
+        background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "dada_2",
+        name: "DADA SILVA",
+        version: "Brasil",
+        rarity: "Especial",
+        rating: 91,
+        position: "ST",
+        secondaryPositions: [],
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/br.png",
+        teamIcon: "teams/Chelsea.png",
+        image: "assets/characters/Brasil/Dada2.png",
+        background: "assets/Cartas/Brasil.png"
+    },
+    {
+        id: "dada_shape",
+        name: "DADA SILVA",
+        version: "Shapesifters",
+        rarity: "Especial",
+        rating: 93,
+        position: "CB",
+        secondaryPositions: [],
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/br.png",
+        teamIcon: "teams/Chelsea.png",
+        image: "assets/characters/Brasil/DadaShape.png",
+        background: "assets/Cartas/Shapesifters.png"
     }
 ];
