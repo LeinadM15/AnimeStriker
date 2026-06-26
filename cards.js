dd53f8b0e669e9264c740796fd971cd52cbfc639
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-06-26 15:19:57 +0200

    Add Adam Blake cards

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const DB_VERSION = '284';
+const DB_VERSION = '285';
 
 const tsubasaAll = [
     ...tsubasaCards,
--- database/tsubasa_cards.js
+++ database/tsubasa_cards.js
@@ -12581,5 +12581,47 @@ const nathanCards = [
         teamIcon: "teams/RealSociedad.png",
         image: "assets/characters/Shawn/ShawnLegen.png",
         background: "assets/Cartas/Icono.png"
+    },
+    {
+        id: "adam_blake_oro",
+        name: "ADAM BLAKE",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 89,
+        position: "ST",
+        secondaryPositions: [],
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/gb-eng.png",
+        teamIcon: "teams/Tottenham.png",
+        image: "assets/characters/Inglaterra/AdamBlakeOro.png",
+        background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "adam_blake_centurion",
+        name: "ADAM BLAKE",
+        version: "Centurion",
+        rarity: "Especial",
+        rating: 92,
+        position: "ST",
+        secondaryPositions: [],
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/gb-eng.png",
+        teamIcon: "teams/Tottenham.png",
+        image: "assets/characters/Inglaterra/AdamBlake.png",
+        background: "assets/Cartas/Centurion.png"
+    },
+    {
+        id: "adam_blake_prime",
+        name: "ADAM BLAKE",
+        version: "Trophy",
+        rarity: "Especial",
+        rating: 94,
+        position: "ST",
+        secondaryPositions: [],
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/gb-eng.png",
+        teamIcon: "teams/Tottenham.png",
+        image: "assets/characters/Inglaterra/AdamBlakePrime.png",
+        background: "assets/Cartas/Trophy.png"
     }
 ];
