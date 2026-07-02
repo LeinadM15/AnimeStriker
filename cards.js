9d0e6ec4694dbdc7d7203882d97dc32cb3b6dca7
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-02 21:41:21 +0200

    Adds Zeus team and all Byron Love variants

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const DB_VERSION = '1783015981776';
+const DB_VERSION = '1783021267914';
 
 const tsubasaAll = [
     ...tsubasaCards,
@@ -59,7 +59,8 @@ const tsubasaAll = [
     ...otakuCards,
     ...shurikenCards,
     ...farmCards,
-    ...kirkwoodCards
+    ...kirkwoodCards,
+    ...zeusCards
 ];
 tsubasaAll.forEach(c => c._series = 'tsubasa');
 
