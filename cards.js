c1123ee78b8f0758a4bc5e8df07c82d6cf74ff88
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-04 01:12:19 +0200

    Add Ikki Niko (4 versions)

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const DB_VERSION = '1783134600000';
+const DB_VERSION = '1783144600000';
 
 const tsubasaAll = [
     ...tsubasaCards,
@@ -87,7 +87,8 @@ const bluelockAll = [
     ...franciaBLCards,
     ...franciaHieloCards,
     ...kunigamiCards,
-    ...saeCards
+    ...saeCards,
+    ...nikoCards
 ];
 bluelockAll.forEach(c => c._series = 'bluelock');
 
---
+++ database/niko_cards.js
@@ -0,0 +1,62 @@
+const nikoCards = [
+    {
+        id: 'niko_oro',
+        name: 'IKKI NIKO',
+        version: 'Oro',
+        rarity: 'Oro',
+        rating: 85,
+        position: 'CB',
+        secondaryPositions: ['CDM', 'RB', 'LB'],
+        league: 'Serie A',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Inter.png',
+        image: 'assets/characters/Niko/NikoOro.png',
+        background: 'assets/Cartas/Oro.png'
+    },
+    {
+        id: 'niko_ubers',
+        name: 'IKKI NIKO',
+        version: 'Ubers',
+        rarity: 'Ubers',
+        rating: 88,
+        position: 'CB',
+        secondaryPositions: ['CDM', 'RB', 'LB'],
+        league: 'Serie A',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Ubers.png',
+        image: 'assets/characters/Niko/Niko.png',
+        background: 'assets/Cartas/Ubers.png'
+    },
+    {
+        id: 'niko_pase',
+        name: 'IKKI NIKO',
+        version: 'Pase',
+        rarity: 'Inter',
+        rating: 89,
+        position: 'CB',
+        secondaryPositions: ['CDM', 'RB', 'LB'],
+        league: 'Serie A',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Inter.png',
+        image: 'assets/characters/Niko/NikoPase.png',
+        background: 'assets/Cartas/Inter.png'
+    },
+    {
+        id: 'niko_prime',
+        name: 'IKKI NIKO',
+        version: 'Prime',
+        rarity: 'Tots',
+        rating: 91,
+        position: 'CB',
+        secondaryPositions: ['CDM', 'RB', 'LB'],
+        league: 'Serie A',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Inter.png',
+        image: 'assets/characters/Niko/NikoPrime.png',
+        background: 'assets/Cartas/Tots.png'
+    }
+];
+
+if (typeof window !== 'undefined') {
+    window.nikoCards = nikoCards;
+}
