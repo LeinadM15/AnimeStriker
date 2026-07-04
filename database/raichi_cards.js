c3388496aca091426b8072eed1f5349bc6b18b07
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-04 14:48:50 +0200

    Add Raichi cards

---
+++ database/raichi_cards.js
@@ -0,0 +1,67 @@
+const raichiCards = [
+    {
+        id: "raichi_oro",
+        name: "RAICHI",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 85,
+        position: "CDM",
+        secondaryPositions: ["CB"],
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/WerderBremen.png",
+        image: "assets/characters/Raichi/RaichiOro.png"
+    },
+    {
+        id: "raichi_rojiazul",
+        name: "RAICHI",
+        version: "RojiAzul",
+        rarity: "Especial",
+        rating: 88,
+        position: "CDM",
+        secondaryPositions: ["CB"],
+        league: "Blue Lock",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/BluLock.png",
+        image: "assets/characters/Raichi/RaichiBlu.png"
+    },
+    {
+        id: "raichi_centurion",
+        name: "RAICHI",
+        version: "Centurion",
+        rarity: "Especial",
+        rating: 89,
+        position: "CDM",
+        secondaryPositions: ["CB"],
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/WerderBremen.png",
+        image: "assets/characters/Raichi/Raichi.png"
+    },
+    {
+        id: "raichi_bastard",
+        name: "RAICHI",
+        version: "Bastard",
+        rarity: "Especial",
+        rating: 90,
+        position: "CDM",
+        secondaryPositions: ["CB"],
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/Bastard.png",
+        image: "assets/characters/Raichi/Raichiseg.png"
+    },
+    {
+        id: "raichi_prime",
+        name: "RAICHI",
+        version: "Scream",
+        rarity: "Especial",
+        rating: 91,
+        position: "CDM",
+        secondaryPositions: ["CB"],
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/WerderBremen.png",
+        image: "assets/characters/Raichi/RaichiPrime.png"
+    }
+];
