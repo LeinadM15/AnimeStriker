b140e8517310054b88942ad511dfd05a77d31618
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-04 13:19:36 +0200

    Fix cards.js syntax error and bump DB_VERSION

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const ;
+const DB_VERSION = '1783163959380';
 
 const tsubasaAll = [
     ...tsubasaCards,
