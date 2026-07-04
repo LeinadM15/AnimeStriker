1e38227aa10b84b7fabbee17216821437dc6b2db
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-04 13:13:48 +0200

    Add Gabi cards

---
+++ database/gabi_cards.js
@@ -0,0 +1,118 @@
+const gabiCards = [
+    {
+        id: 'gabi_oro',
+        name: 'GABI',
+        version: 'Oro',
+        rarity: 'Oro',
+        rating: 84,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'J-League',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Raimon.png',
+        image: 'assets/characters/Gabi/GabiOro.png',
+        background: 'assets/Cartas/Oro.png'
+    },
+    {
+        id: 'gabi_mix_oro',
+        name: 'GABI',
+        version: 'MixOro',
+        rarity: 'Oro',
+        rating: 88,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'Ligue 1',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/PXG.png',
+        image: 'assets/characters/Gabi/GabiMixOro.png',
+        background: 'assets/Cartas/Oro.png'
+    },
+    {
+        id: 'gabi_nieebla',
+        name: 'GABI',
+        version: 'Nieebla',
+        rarity: 'Raimon',
+        rating: 87,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'J-League',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Raimon.png',
+        image: 'assets/characters/Gabi/GabiNieebla.png',
+        background: 'assets/Cartas/Raimon.png'
+    },
+    {
+        id: 'gabi_juana',
+        name: 'GABI',
+        version: 'Juana',
+        rarity: 'PXG',
+        rating: 90,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'Ligue 1',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/PXG.png',
+        image: 'assets/characters/Gabi/GabiJuana.png',
+        background: 'assets/Cartas/PXG.png'
+    },
+    {
+        id: 'gabi_brunilda',
+        name: 'GABI',
+        version: 'Brunilda',
+        rarity: 'FutBirthday',
+        rating: 89,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'J-League',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Raimon.png',
+        image: 'assets/characters/Gabi/GabiBrunilda.png',
+        background: 'assets/Cartas/FutBirthday.png'
+    },
+    {
+        id: 'gabi_arm',
+        name: 'GABI',
+        version: 'Armadura',
+        rarity: 'Azul',
+        rating: 91,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'J-League',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Raimon.png',
+        image: 'assets/characters/Gabi/GabiArm.png',
+        background: 'assets/Cartas/Azul.png'
+    },
+    {
+        id: 'gabi_mix',
+        name: 'GABI',
+        version: 'Tots',
+        rarity: 'Tots',
+        rating: 93,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'J-League',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/Raimon.png',
+        image: 'assets/characters/Gabi/Gabimix.png',
+        background: 'assets/Cartas/Tots.png'
+    },
+    {
+        id: 'gabi_prime',
+        name: 'GABI',
+        version: 'Prime',
+        rarity: 'Naranja',
+        rating: 94,
+        position: 'CB',
+        secondaryPositions: ['LB', 'LWB', 'CDM'],
+        league: 'Ligue 1',
+        nationFlag: 'https://flagcdn.com/w40/jp.png',
+        teamIcon: 'teams/PXG.png',
+        image: 'assets/characters/Gabi/GabiPrime.png',
+        background: 'assets/Cartas/Naranja.png'
+    }
+];
+
+if (typeof window !== 'undefined') {
+    window.gabiCards = gabiCards;
+}
