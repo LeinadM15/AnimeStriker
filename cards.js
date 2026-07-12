b0f768bcc30da324d5e41d84057d2747b956d876
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-12 12:23:35 +0200

    Bump DB_VERSION to fix Sporting cache

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const DB_VERSION = '1783851524336';
+const DB_VERSION = '1783851814481';
 
 const tsubasaAll = [
     ...tsubasaCards,
