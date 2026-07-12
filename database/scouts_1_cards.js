f360535e7a2b8e180e7bb9373be99fb634174d9f
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-12 12:33:44 +0200

    Add 4 Scouts1 Japanese players to Bundesliga

---
+++ database/scouts_1_cards.js
@@ -0,0 +1,54 @@
+const scoutsOneCards = [
+    {
+        id: "Andy",
+        name: "ANDY",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 75,
+        position: "CB",
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/Koeln.png",
+        image: "assets/characters/Scouts1/Andy.png",
+        background: "assets/Cartas/Orobase.png"
+    },
+    {
+        id: "AureliaDingle",
+        name: "AURELIA DINGLE",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 74,
+        position: "CM",
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/Schalke.png",
+        image: "assets/characters/Scouts1/AureliaDingle.png",
+        background: "assets/Cartas/Orobase.png"
+    },
+    {
+        id: "JimmyMach",
+        name: "JIMMY MACH",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 77,
+        position: "ST",
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/Monchengladbach.png",
+        image: "assets/characters/Scouts1/JimmyMach.png",
+        background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "TonyHacker",
+        name: "TONY HACKER",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 76,
+        position: "CM",
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/jp.png",
+        teamIcon: "teams/Leipzig.png",
+        image: "assets/characters/Scouts1/TonyHacker.png",
+        background: "assets/Cartas/Oro.png"
+    }
+];
