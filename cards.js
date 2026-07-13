aad32264a9dab92467e97013056a818538f69ab5
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-13 14:42:06 +0200

    Add Ryusei Shidou cards

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const DB_VERSION = '1783897078736';
+const DB_VERSION = '1783946520271';
 
 const tsubasaAll = [
     ...tsubasaCards,
@@ -132,7 +132,8 @@ const bluelockAll = [
     ...kunigamiCards,
     ...saeCards,
     ...nikoCards,
-    ...kuronaCards
+    ...kuronaCards,
+    ...shidouCards
 ];
 bluelockAll.forEach(c => c._series = 'bluelock');
 
