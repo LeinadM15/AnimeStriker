2946397b8ac9058747badd3c553afc7661a37b15
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-13 23:09:00 +0200

    Add Pierre and Napoleon new cards, fix Gabi and Shidou leagues

--- database/napoleon_cards.js
+++ database/napoleon_cards.js
@@ -36,6 +36,19 @@ const napoleonCards = [
         teamIcon: "teams/Marsella.png",
         image: "assets/characters/Francia/NapoleonOro.png",
         background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "napoleon_prime",
+        name: "NAPOLEON",
+        version: "Prime",
+        rarity: "Especial",
+        rating: 94,
+        position: "ST",
+        league: "Superliga Europea",
+        nationFlag: "https://flagcdn.com/w40/fr.png",
+        teamIcon: "teams/Marsella.png",
+        image: "assets/characters/Francia/NapoleonPrime.png",
+        background: "assets/backgrounds/Fondo_NapoleonPrime.jpg"
     }
 ];
 
--- database/pierre_cards.js
+++ database/pierre_cards.js
@@ -25,9 +25,33 @@ const pierreCards = [
         teamIcon: "teams/PXG.png",
         image: "assets/characters/Francia/PierreOro.png",
         background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "pierre_prime",
+        name: "PIERRE",
+        version: "Prime",
+        rarity: "Especial",
+        rating: 95,
+        position: "CAM",
+        league: "Superliga Europea",
+        nationFlag: "https://flagcdn.com/w40/fr.png",
+        teamIcon: "teams/PXG.png",
+        image: "assets/characters/Francia/PierrePrime.png",
+        background: "assets/backgrounds/Fondo_PierrePrime.jpg"
+    },
+    {
+        id: "pierre_especial",
+        name: "PIERRE",
+        version: "Especial",
+        rarity: "Especial",
+        rating: 93,
+        position: "CAM",
+        league: "Superliga Europea",
+        nationFlag: "https://flagcdn.com/w40/fr.png",
+        teamIcon: "teams/PXG.png",
+        image: "assets/characters/Francia/Pierre.png",
+        background: "assets/backgrounds/Fondo_Pierre.png"
     }
 ];
 
-// ==========================================
-// NAPOLEON (LOUIS)
-// ==========================================
+window.pierreCards = pierreCards;
