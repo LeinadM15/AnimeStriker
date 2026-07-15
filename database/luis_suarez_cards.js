c6fac03d98559eeca4289b2bd9407ce17c3e874d
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-16 01:10:12 +0200

    Add Luis Suarez cards

---
+++ database/luis_suarez_cards.js
@@ -0,0 +1,28 @@
+const luisSuarezCards = [
+    {
+        id: "luis_suarez_oro",
+        name: "LUIS SUAREZ",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 89,
+        position: "ST",
+        league: "La Liga",
+        nationFlag: "https://flagcdn.com/w40/uy.png",
+        teamIcon: "teams/Barcha.png",
+        image: "assets/characters/Uruguay/LuisSuarezOro.png",
+        background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "luis_suarez_icono",
+        name: "LUIS SUAREZ",
+        version: "Icono",
+        rarity: "Especial",
+        rating: 94,
+        position: "ST",
+        league: "La Liga",
+        nationFlag: "https://flagcdn.com/w40/uy.png",
+        teamIcon: "teams/Barcha.png",
+        image: "assets/characters/Uruguay/LuisSuarez.png",
+        background: "assets/Cartas/Icono.png"
+    }
+];
