0e1d7f16c2dd3502f2ade3f62378c3497e233871
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-16 16:11:37 +0200

    Add Carwyn to Manshine

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 // cards.js - Catálogo Maestro
 // Este archivo reúne todos los arrays definidos en los archivos de la carpeta database/ y asigna la serie
 // Bump DB_VERSION whenever card data changes to invalidate draft localStorage cache
-const DB_VERSION = '1784210702319';
+const DB_VERSION = '1784211097106';
 
 const tsubasaAll = [
     ...tsubasaCards,
@@ -115,7 +115,8 @@ const tsubasaAll = [
     ...bosniaCards,
     ...barchaCards,
     ...racingCards,
-    ...greciaCards
+    ...greciaCards,
+    ...inglaterraCards
 ];
 tsubasaAll.forEach(c => c._series = 'tsubasa');
 
---
+++ database/inglaterra_cards.js
@@ -0,0 +1,28 @@
+const inglaterraCards = [
+    {
+        id: "carwyn_oro",
+        name: "CARWYN",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 85,
+        position: "ST",
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/gb-eng.png",
+        teamIcon: "teams/Manshine.png",
+        image: "assets/characters/Inglaterra/CarwynOro.png",
+        background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "carwyn_manshine",
+        name: "CARWYN",
+        version: "Manshine",
+        rarity: "Especial",
+        rating: 88,
+        position: "ST",
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/gb-eng.png",
+        teamIcon: "teams/Manshine.png",
+        image: "assets/characters/Inglaterra/Carwyn.png",
+        background: "assets/Cartas/Manshine.png"
+    }
+];
