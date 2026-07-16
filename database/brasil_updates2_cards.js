89b3ba6fd13366cdd72f78a783ed9fccfb8c030c
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-17 00:06:18 +0200

    Add Victor Oro and Victor Brasil

---
+++ database/brasil_updates2_cards.js
@@ -0,0 +1,28 @@
+const brasilUpdates2Cards = [
+    {
+        id: "victor_oro",
+        name: "VICTOR",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 84,
+        position: "CM",
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/br.png",
+        teamIcon: "teams/Blackburn.png",
+        image: "assets/characters/Brasil/VictorOro.png",
+        background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "victor_brasil",
+        name: "VICTOR",
+        version: "Brasil",
+        rarity: "Especial",
+        rating: 87,
+        position: "CM",
+        league: "Premier League",
+        nationFlag: "https://flagcdn.com/w40/br.png",
+        teamIcon: "teams/Blackburn.png",
+        image: "assets/characters/Brasil/Victor.png",
+        background: "assets/Cartas/Brasil.png"
+    }
+];
