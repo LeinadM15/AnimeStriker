ef1c8a0a6b430f45b232fa20c51539ca21e11c88
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-16 15:38:40 +0200

    Add Dzeko from Bosnia

---
+++ database/bosnia_cards.js
@@ -0,0 +1,15 @@
+const bosniaCards = [
+    {
+        id: "dzeko_oro",
+        name: "DZEKO",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 85,
+        position: "ST",
+        league: "Serie A",
+        nationFlag: "https://flagcdn.com/w40/ba.png",
+        teamIcon: "teams/Roma.png",
+        image: "assets/characters/Bosnia/Dzeko.png",
+        background: "assets/Cartas/Oro.png"
+    }
+];
