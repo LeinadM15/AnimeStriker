3572e41312efda1536226c3ea223c2ba30fe63be
LeinadM15 <leinadmarulandasanchez@gmail.com>
2026-07-20 14:08:39 +0200

    Add Oliver Hahn cards (Oro and Icono)

--- database/alemania_updates3_cards.js
+++ database/alemania_updates3_cards.js
@@ -11,5 +11,31 @@ const alemaniaUpdates3Cards = [
         teamIcon: "teams/WerderBremen.png",
         image: "assets/characters/Alemania/MargusPrime.png",
         background: "assets/backgrounds/Fondo_Margus.jpg"
+    },
+    {
+        id: "oliverhahn_oro",
+        name: "OLIVER HAHN",
+        version: "Oro",
+        rarity: "Oro",
+        rating: 83,
+        position: "GK",
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/de.png",
+        teamIcon: "teams/Bastard.png",
+        image: "assets/characters/Alemania/OliverHahnOro.png",
+        background: "assets/Cartas/Oro.png"
+    },
+    {
+        id: "oliverhahn_icono",
+        name: "OLIVER HAHN",
+        version: "Icono",
+        rarity: "Especial",
+        rating: 91,
+        position: "GK",
+        league: "Bundesliga",
+        nationFlag: "https://flagcdn.com/w40/de.png",
+        teamIcon: "teams/Bastard.png",
+        image: "assets/characters/Alemania/OliverHahn.png",
+        background: "assets/Cartas/Icono.png"
     }
 ];
